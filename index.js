cff1e5d0b84b81b02f20a710a0f7b8f411c64003
Arthur Zorzo <arthurcorteszorzo@gmail.com>
2022-10-09 21:49:25 -0300

    adicionada a funcao que trata o erro

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'Erro na chamada do arquivo'));
+}
+
 function chamaArquivo(caminhoDoArquivo){
     const encoding = 'UTF-8';
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if(erro){
+            trataErro(erro);
+        }
         console.log(chalk.green(texto));
     })
 }
